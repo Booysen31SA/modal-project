3772562b9bb7c804b52a1f29ec8b284db83318bd
Matthew Booysen <MatthewBoo@AWARDS.CO.ZA>
2022-06-20 16:04:28 +0200

    added fetch json data

--- src/views/Jobs/JobDetail.vue
+++ src/views/Jobs/JobDetail.vue
@@ -1,19 +1,32 @@
 <template>
-  <h1>Job Details page</h1>
-  <p>The Job id is {{ id }}</p>
+  <div v-if="job">
+    <h1>Job Details page</h1>
+    <h1>{{ job.title }}</h1>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    Loading job details...
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['id'],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // }
-}
+  props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => {
+        this.job = data;
+      })
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
 <style>
-
 </style>
